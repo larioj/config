f84a0e51c868443a5290b6634783e4887271c128
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2015-09-24 18:37:58 -0700

    setup

--- setup.sh
+++ setup.sh
@@ -1,27 +1,45 @@
 #! /bin/bash
 # Script must be run from the DotFiles directory.
 
-# Output options
-showEvents="false"
+# Control Flow
+showEvents="true"
+actionDic=(
+"setup:makeLinks"
+"cleanup:removeLinks"
+)
+
+usage () {
+  echo "usage: $0 action"
+}
+
+getAction () {
+  action="usage"
+  for item in ${actionDic[*]}
+  do
+    if [ "$1" == "$(key $item)" ]; then
+      action="$(val $item)"
+    fi
+  done
+  printf "%s" $action
+}
 
 sourceD=$(pwd)
 destD=$HOME
 sourceFiles=(
+# "setup.sh:.setup.sh"
 "bash_aliases:.bash_aliases"
 "bash_profile:.bash_profile"
-"cleanup.sh:.cleanup.sh"
 "emacs:.emacs"
 "gitconfig:.gitconfig"
 "haskeline:.haskeline"
-"setup.sh:.setup.sh"
 "vimrc:.vimrc"
 )
 
-snd () {
+val () {
   printf "%s" ${1##*:}
 }
 
-fst () {
+key () {
   printf "%s" ${1%%:*}
 }
 
@@ -35,15 +53,20 @@ showOrRun () {
 makeLinks () {
   for item in ${sourceFiles[*]}
   do
-    showOrRun "ln -s $sourceD/$(fst $item) $destD/$(snd $item)"
+    showOrRun "ln -s $sourceD/$(key $item) $destD/$(val $item)"
   done
 }
 
 removeLinks () {
   for item in ${sourceFiles[*]}
   do
-    showOrRun "rm $destD/$(snd $item)"
+    showOrRun "rm $destD/$(val $item)"
   done
 }
 
-removeLinks
+main () {
+  action="$(getAction $1)"
+  $action $@
+}
+
+main $@
