515a7f74cb9cde3ee87cf1f747af504bccb728fd
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-03-06 10:07:27 -0800

    fol

--- folders.sh
+++ folders.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
-git clone https://github.com/larioj/bide.git ~
-git clone https://github.com/larioj/vim.git ~
+cd ~
+
+git clone --recursive https://github.com/larioj/bide.git
+cd bide
+./install.sh
+
+git clone --recursive https://github.com/larioj/vim.git
+rm -rf .vim
+mv vim .vim
 
